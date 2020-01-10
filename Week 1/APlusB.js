69010e8102414103b9fcbe8570ed01f8667f6698
Adrian Ross <adriantoddross@gmail.com>
2020-01-10 11:58:26 -0500

    refactor code to avoid double .split()s

--- Week 1/APlusB.js
+++ Week 1/APlusB.js
@@ -19,9 +19,8 @@ rl.on('line', readLine);
 
 function readLine (line) {
   if (line !== "\n") {
-    var a = parseInt(line.toString().split(' ')[0], 10);
-    var b = parseInt(line.toString().split(' ')[1], 10);
-    console.log(a + b);
+    var integers = line.toString().split(' ');
+    console.log(parseInt(integers[0], 10) + parseInt(integers[1], 10));
     process.exit();
   }
 }
